48e266d30083e3e715278931f7726560f95bd2d3
M-RI7AL-2002 <mouhamedrihal123@gmail.com>
2025-12-11 22:10:42 +0100

    added apps

---
+++ hackatoonBackend/appAthlet/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppathletConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "appAthlet"
--- hackatoonBackend/hackatoonBackend/settings.py
+++ hackatoonBackend/hackatoonBackend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appAI.apps.AppaiConfig',
+    'appAthlet.apps.AppathletConfig',
+    'appAdmin.apps.AppadminConfig',
 ]
 
 MIDDLEWARE = [
